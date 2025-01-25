647d0b49196aecbffeadb8815118406f7eb1bf3d
Joyan9 <83577461+Joyan9@users.noreply.github.com>
2025-01-25 15:34:38 +0000

    Functions completed

--- Work/pcost.py
+++ Work/pcost.py
@@ -10,19 +10,23 @@ and calculates how much it cost to purchase all of the shares in the portfolio.
 Hint: to convert a string to an integer, use int(s). To convert a string to a floating point, 
 use float(s).
 '''
-cost = 0
-f = open('/workspaces/practical-python/Work/Data/portfolio.csv', 'rt')
-'''
-r: This stands for "read mode," meaning the file is opened for reading. The file must exist; otherwise, an error will occur.
-t: This stands for "text mode," meaning the file is opened as a text file (as opposed to binary mode).
-'''
-headers = next(f).split(',')
-for line in f:
-    row = line.split(',')
-    price = float(row[-1])
-    shares = float(row[1])
-    cost = cost + (shares*price)
-print(cost)
-f.close()
 
+def portfolio_cost(filename):
+    cost = 0
+    f = open(filename, 'rt')
+    '''
+    r: This stands for "read mode," meaning the file is opened for reading. The file must exist; otherwise, an error will occur.
+    t: This stands for "text mode," meaning the file is opened as a text file (as opposed to binary mode).
+    '''
+    headers = next(f).split(',')
+    for line in f:
+        row = line.split(',')
+        price = float(row[-1])
+        shares = float(row[1])
+        cost = cost + (shares*price)
+    f.close()
+    return cost
+
+cost = portfolio_cost('/workspaces/practical-python/Work/Data/portfolio.csv')
+print('Total cost:', cost)
 
